0c151d39f2760b2e52d0093f7a71c3faaf442168
Andrew Kibiwott <kibiandrew@gmail.com>
2022-12-16 12:43:53 +0300

    at work

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-async function getHolidays() {
-  try {
-    const response = await fetch('https://public-holiday.p.rapidapi.com/2019/US', options);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error(error);
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '177082c668msh5fdc9f437358997p1d112fjsn87d2c450ff01',
+    'X-RapidAPI-Host': 'public-holiday.p.rapidapi.com'
   }
-}
+};
 
-getHolidays();
+fetch('https://public-holiday.p.rapidapi.com/2019/US', options)
+  .then(response => response.json())
+  .then(response => {
+    // Get the table element
+    const table = document.getElementById('table');
+
+    // Get the keys from the first object in the response array
+    const headers = Object.keys(response[0]);
+
+    // Create the table header row
+    const headerRow = document.createElement('tr');
+    headers.forEach(header => {
+      const th = document.createElement('th');
+      th.textContent = header;
+      headerRow.appendChild(th);
+    });
+    table.appendChild(headerRow);
+
+    // Create a row for each object in the response array
+    response.forEach(obj => {
+      const row = document.createElement('tr');
+      headers.forEach(header => {
+        const td = document.createElement('td');
+        td.textContent = obj[header];
+        row.appendChild(td);
+      });
+      table.appendChild(row);
+    });
+  })
+  .catch(err => console.error(err));
